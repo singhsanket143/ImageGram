036b4e00f5df458f21da6d429f0292b08abb5942
Sanket Singh <singhsanket143@gmail.com>
2024-10-05 12:48:21 +0530

    Adde service layer structure

---
+++ src/services/postService.js
@@ -0,0 +1,9 @@
+export const createPost = async (createPostObejct) => {
+    // 1. Take the image of the post and upload on aws
+
+    // 2. Get the url of the image from the aws response
+
+    // 3. Create a post with the caption and the image url in the db using repository
+
+    // 4. Return the post object
+}
