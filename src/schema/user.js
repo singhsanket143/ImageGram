2b05a58dc10260b092bf8a6d1dca7b3e70e858d3
Sanket Singh <singhsanket143@gmail.com>
2024-10-05 11:57:59 +0530

    Added basic user schema

---
+++ src/schema/user.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 5
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 5,
+        validate: {
+            validator: function (emailValue) {
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailValue);
+            },
+            message: 'Invalid email format'
+        },
+        
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 5
+    }
+}, { timestamps: true });
+
+const user = mongoose.model("User", userSchema); // user collection
+
+export default user;
